de399263d1f905b4b1565c16ad69709b6afa0961
Both Sann <thannsopheakboth@gmail.com>
2024-11-29 01:05:11 +0700

    adding animation

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const icons = document.querySelectorAll(".section-1__icons .fas");
+
+setInterval(function () {
+  const activeIcon = document.querySelector(".section-1__icons .show");
+
+  if (activeIcon) {
+    // console.log(activeIcon);
+    activeIcon.classList.remove("show");
+  }
+
+  let nextIcon;
+  nextIcon = activeIcon.nextElementSibling || icons[0];
+  nextIcon.classList.add("show");
+
+  // console.log(nextIcon);
+}, 2000);
